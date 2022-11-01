56a7c4f2404fb3f45f85b65080b1e83c06f82ec1
Zac Gladman <zacharygladman@yahoo.co.uk>
2022-11-01 10:21:09 +0000

    Write pseudocode for KVP kata

---
+++ src/key-value-pairs.ts
@@ -0,0 +1,27 @@
+/*Write a function, `printAllKeyValuePairs(obj)` which takes an object with unknown content and prints to console, one per line, each **key** and **value** in the object.
+
+const characters = {
+  Bruce: "Hulk",
+  Peter: "Spiderman",
+  Ororo: "Storm"
+};
+
+the expected output would be
+
+Bruce Hulk
+Peter Spiderman
+Ororo Storm
+
+
+@params: object
+@returns: void
+
+PROGRAMME printAllKeyValuePairs(CHARACTERS):
+    FOR EACH KEY_VALUE_PAIR in CHARACTERS:
+        PRINT KEY + '' + VALUE
+    END FOR.
+END.
+
+*/
+
+
